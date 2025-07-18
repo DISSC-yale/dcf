e79616b202482a31534f9ffd8e20479f4ab0369f
Micah <micah.iserman@gmail.com>
2025-07-18 05:54:35 -0400

    adds total and Virgin Islands population

--- build.R
+++ build.R
@@ -7,11 +7,26 @@ devtools::check()
 covr::report(covr::package_coverage(quiet = FALSE), "docs/coverage.html")
 
 # update sysdata.rda
-population <- dcf::dcf_load_census(2021, "resources")
+population <- dcf::dcf_load_census(2021L, "resources")
+population <- rbind(
+  population,
+  rbind(
+    c(
+      GEOID = "0",
+      region_name = "Total",
+      colSums(population[, -(1L:2L)])
+    ),
+    c(
+      GEOID = "52",
+      region_name = "Virgin Islands",
+      Total = 87146L
+    )[colnames(population)]
+  )
+)
 epic_id_maps <- list(
   regions = structure(population$GEOID, names = population$region_name),
   months = structure(
-    formatC(seq_len(12), width = 2L, flag = "0"),
+    formatC(seq_len(12L), width = 2L, flag = "0"),
     names = month.abb
   )
 )
