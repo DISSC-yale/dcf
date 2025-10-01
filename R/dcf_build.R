ad10e3601a862ab9f447c5b94aea22c004c2d3f4
Micah <micah.iserman@gmail.com>
2025-10-01 05:09:09 -0400

    adds separate file log output

--- R/dcf_build.R
+++ R/dcf_build.R
@@ -8,6 +8,7 @@
 #' manually.
 #' @param ... Passes arguments to \code{\link{dcf_process}}.
 #' @param make_diagram Logical; if \code{FALSE}, will not make a \code{status.md} diagram.
+#' @param make_file_log Logical; if \code{FALSE}, will not make a \code{file_log.json} output.
 #' @returns A version of the project report, which is also written to
 #' \code{project_dir/docs/report.json.gz}.
 #' @examples
@@ -21,7 +22,8 @@ dcf_build <- function(
   project_dir = ".",
   is_auto = TRUE,
   ...,
-  make_diagram = TRUE
+  make_diagram = TRUE,
+  make_file_log = TRUE
 ) {
   settings <- dcf_read_settings(project_dir)
   data_dir <- paste0(project_dir, "/", settings$data_dir)
@@ -78,7 +80,32 @@ dcf_build <- function(
       dataframe = "columns"
     )
   } else {
-    report <- invisible(jsonlite::read_json(report_file))
+    report <- jsonlite::read_json(report_file)
+  }
+  if (make_file_log) {
+    file_log <- list()
+    for (file_dir in names(report$metadata)) {
+      if (grepl("/dist", file_dir, fixed = TRUE)) {
+        p <- report$metadata[[file_dir]]
+        for (p_file in p$resources) {
+          file_log[[paste0(
+            settings$data_dir,
+            "/",
+            file_dir,
+            "/",
+            p_file$filename
+          )]] <- list(
+            updated = p_file$last_modified,
+            md5 = p_file$md5
+          )
+        }
+      }
+    }
+    jsonlite::write_json(
+      file_log,
+      paste0(project_dir, "/file_log.json"),
+      auto_unbox = TRUE
+    )
   }
   if (make_diagram) {
     dcf_status_diagram(project_dir)
